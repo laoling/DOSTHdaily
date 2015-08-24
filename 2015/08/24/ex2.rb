3a148955904d84df77a22f05bd9662a4610e0e1f
老零 <838618014@qq.com>
2015-08-24 17:37:15 +0800

    [ADD] some new ruby starter code

---
+++ 2015/08/24/ex2.rb
@@ -0,0 +1,9 @@
+# 注释（comment）可以帮助你以后重读你的代码
+# 在#符号后面的内容会被Ruby忽略
+
+puts "I could have code like this."  # 注释也可以这样写在后面
+
+# 调试之后也可以在代码前加#符号使这段代码片段不再解释
+# print "This won't run."
+
+puts "This will run."
